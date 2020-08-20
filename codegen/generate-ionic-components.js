0d4c2c0112e11619c3c538f585cbc6e7b4d43766
Tim de Putter <tim.de.putter83@googlemail.com>
2020-08-20 08:29:33 +0200

    alert componente generieren

--- codegen/generate-ionic-components.js
+++ codegen/generate-ionic-components.js
@@ -1,52 +1,133 @@
 const fs = require('fs');
 const util = require('util');
+const Parser = require('ts-types-parser');
+const parser = new Parser;
 
-
-var generateIonicTypes = (isJs) => {
+var generateIonicTypes = async (isJs) => {
     printHeader(isJs);
-    withFileDo('./node_modules/@types/react/index.d.ts', generateGenericRowTypes(isJs));
-};
-
-const generateGenericRowTypes = (isJs) => (data) => {
-    generateGenericRowType(isJs, data, "HTMLAttributes", 1);
-    generateGenericRowType(isJs, data, "AriaAttributes", 1);
-    generateGenericRowType(isJs, data, "DOMAttributes", 2);
-    console.log("type IonicBaseAttributes r = HTMLAttributes + AriaAttributes + DOMAttributes + r");
+    await generateAlertComponent("ionAlert", isJs);
 };
 
 
-const generateGenericRowType = (isJs, data, type, closingBracesCount) => {
-    var lines = data.split(/\r?\n/).map(l => l.trim()).map(e => removeEndOfLineComment(e));
-    var region = limitToRegion(lines, type, closingBracesCount);
-    var rowTypeElements = getRowTypeElements(region);
-    printRowType(type, rowTypeElements);
+const generateAlertComponent = async (componentName, isJs) => {
+    var lowerName = componentName;
+    var upperName = upperFist(componentName);
+    if(isJs){
+        genJavascriptCode(lowerName);
+        return;
+    }
+    var lines = (await withFileDo(`./node_modules/@ionic/react/dist/types/components/${upperName}.d.ts`)).split("\n");
+    printRowType(`${upperName}Props`, flatten([], [await parseInterfaceOptions(lines), 
+        await parseControllerProps(lines), parseRefAttributes(lines)]), false);
+    generateComponentFunc(lowerName);
 };
 
-const printRowType = (type, rowTypeElements) => {
-    console.log(`type ${type} r = (`)
+const parseControllerProps = async (lines) => {
+    if(lines.some(l => l.includes("import(\"./createControllerComponent\").ReactControllerProps"))){
+        var data = await withFileDo("./node_modules/@ionic/react/dist/types/components/createControllerComponent.d.ts");    
+        return await getRowTypeElements("ReactControllerProps", getLineData(data), 1);
+    }
+    return [];
+};
+
+const parseRefAttributes = (lines) => {
+    if(lines.some(l => l.includes(".RefAttributes<"))){
+        return ["    key :: String |+| Number"];
+    }
+    return [];
+};
+
+const generateComponentFunc = name => {
+    return console.log(`
+foreign import _${name} :: ReactComponent ${upperFist(name)}Props
+
+${name} :: forall r. Coercible r ${upperFist(name)}Props => r -> JSX
+${name} = element _${name} <<< coerce
+`);    
+};
+
+const parseInterfaceOptions = async (lines) => {
+    var r = await Promise.all(lines.map(async line => {
+        var optionsMatch = /import { (\w+)Options } from '@ionic\/core';/.exec(line);
+        if(optionsMatch){
+            const name = optionsMatch[1];
+            const optName = `${name}Options`;
+            var data = await withFileDo(`./node_modules/@ionic/core/dist/types/components/${name}/${name}-interface.d.ts`);
+            return await getRowTypeElements(optName, getLineData(data), 1, true);
+        }
+        return [];
+    }));
+    return flatten([], r);
+};
+
+const flatten = (initialVal, arr) => {
+    var res = initialVal;
+    arr.forEach(subArr => {
+        res = res.concat(subArr);
+    });
+    return res;
+};
+
+
+
+const getBaseTypeName = (lines, type) => {
+    var extensionDef = `export interface ${type} extends JSXBase.`;
+    return lines.filter(l => l.includes(extensionDef)).map(l => {
+        var start = l.indexOf(extensionDef)+extensionDef.length;
+        return l.slice(start,l.indexOf("<",start))
+    })[0];
+};
+
+const getLineData = (data) => {
+    return data.split(/\r?\n/).map(l => l.trim()).map(e => removeEndOfLineComment(e));
+};
+
+const getStatements = (data) => {
+    return data.split(";").map(l => l.trim()).map(e => removeEndOfLineComment(e));
+};
+
+
+const printRowType = (type, rowTypeElements, generateOpenRow) => {
+    if(generateOpenRow) console.log(`type ${type} r = (`);
+    else console.log(`type ${type} = {`);
     console.log(rowTypeElements.join(",\n"));
-    console.log("| r )");
+    if(generateOpenRow) console.log("| r )\n");
+    else  console.log("}\n");
 };
 
+const getRowTypeElements = async (type, lines, closingBracesCount) => {
+    var baseName = getBaseTypeName(lines, type);
+    if(baseName != undefined) {
+        var data = await withFileDo("./node_modules/@ionic/core/dist/types/stencil-public-runtime.d.ts");
+        return (await getRowTypesFromRegion(type, lines,closingBracesCount)).concat(
+            await getRowTypeElements(baseName, getLineData(data), 1));
+    }
+    return await getRowTypesFromRegion(type, lines, closingBracesCount);
+};
 
-
-
-const getRowTypeElements = (region) => {
-    return region.filter(
+const getRowTypesFromRegion = async (type, lines, closingBracesCount) => {
+    var region = getStatements(flatten("", limitToRegion(lines, type, closingBracesCount)));
+    return await Promise.all(region.filter(
         l => !isEmptyOrSpaces(l)).filter(
-            e => !isComment(e)).filter(e => e.includes("?:")).map(
-                e => parseGenereicRowTypeElement(e)).map(generateRowTypeElement);
+            e => !isComment(e)).filter(e => e.includes(":")).map(
+                e => parseGenereicRowTypeElement(e)).map(generateRowTypeElement(lines)));
+
 };
 
-const generateRowTypeElement = (rowEl) => {
-    return `    ${fixName(rowEl.name)} :: ${generateType(rowEl.type)} |+| Undefined`;
+const generateRowTypeElement = (lines) => async (rowEl) => {
+    return `    ${fixName(rowEl.name)} :: ${await generateType(rowEl.type, lines)} |+| Undefined`;
 };
 
 const fixName = (name) => {
     return name.replace(/-/g,"").replace(/\'/g,"");
 };
 
-const generateType = (typeScriptType) => {
+const generateType = async (typeScriptType, lines ) => {
+    if(typeScriptType.endsWith("[]")) return await generateArrayType(typeScriptType, lines);
+    if(lines.some(l => l.includes(`export interface ${typeScriptType}`))) {
+        await generateRowType(lines, typeScriptType, 1, false);
+        return typeScriptType;
+    }
     if(["string", "CSSProperties", "{"].includes(typeScriptType)) return "String";
     if(["Booleanish", "boolean"].includes(typeScriptType)) return "Boolean";
     if(typeScriptType == "ReadonlyArray<string>") return "(Array String)";
@@ -54,18 +135,33 @@ const generateType = (typeScriptType) => {
     if(typeScriptType == "any") return "String |+| Number |+| Boolean |+| (Array String) |+| (Array Boolean) |+| (Array Number)"
     if(typeScriptType.includes("EventHandler<T>")) return "EventHandler";
     if(typeScriptType == "ReactNode") return "Array JSX";
-    if(typeScriptType.includes("|")) return generateSumType(typeScriptType);
+    if(typeScriptType.includes("|")) return await generateSumType(typeScriptType, lines);
     return "String";
 };
 
-const generateSumType = (typeScriptType) => {
-    const res = distinct(typeScriptType.split("|").map(
-        e => e.trim()).filter(e => e != "inherit").map(e => generateType(e)));
+const generateRowType = async (lines, type, closingBracesCount, generateOpenRow) => {
+    printRowType(type, await getRowTypeElements(type, lines, closingBracesCount), generateOpenRow);
+};
+
+const generateArrayType = async (typeScriptType, lines) => {
+    typeScriptType = typeScriptType.slice(0,-2);
+    if(typeScriptType.startsWith("(") && typeScriptType.endsWith(")")) {
+        typeScriptType = typeScriptType.slice(1,typeScriptType.length).slice(0,-1);
+        return `Array (${await generateType(typeScriptType, lines)})`;
+    };
+    return `Array ${await generateType(typeScriptType, lines)}`;
+};
+
+const generateSumType = async (typeScriptType, lines) => {
+    const res = await Promise.all(distinct(typeScriptType.split("|").map(
+        e => e.trim()).filter(e => e != "inherit").map(async e => await generateType(e, lines))));
     return res.join(" |+| ");
 };
 
 const parseGenereicRowTypeElement = (codeline) =>  {
-    var s = codeline.split("?:").map(s => s.trim());
+    var elements = codeline.split("?:");
+    if(elements.length == 1) elements = codeline.split(":");
+    var s = elements.map(s => s.trim());
     return {name: s[0], type: s[1].replace(';','') };
 }
 
@@ -79,6 +175,7 @@ const removeEndOfLineComment = (codeline) => {
 
 const limitToRegion = (lines, marker, closingBracesCount) => {
     lines = lines.slice(lines.findIndex(l => l.includes(`interface ${marker}`))+1);
+    if(lines.length > 0 && lines[0].includes('}')) return [];
     return lines.slice(0, findIndexOfNthClosingBrace(lines, closingBracesCount, 0));
 };
 
@@ -88,13 +185,8 @@ const findIndexOfNthClosingBrace = (lines, closingBraceNo, idx) => {
         lines.findIndex((l, i) => i > idx && l.includes("}")));
 };
 
-const withFileDo = (name, action) => {
-    fs.readFile(name, 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        action(data);
-      });
+const withFileDo = async (name) => {
+    return await fs.readFileSync(name, 'utf8');
 };
 
 const distinct = (a) => {
@@ -111,24 +203,27 @@ const printHeader = (isJs) => {
     var Ionic = require("@ionic/react");
     `:`
 module Ionic where
-import React.Basic (JSX)
-import React.Basic.Events (EventHandler)
-import Type.Row (type (+))
-import Untagged.Union (type (|+|))  
+
+import Prelude
+
 import Literals.Undefined (Undefined)
+import React.Basic (JSX, ReactComponent, element)
+import React.Basic.Events (EventHandler)
+import Untagged.Coercible (class Coercible, coerce)
+import Untagged.Union (type (|+|))
+    
     `);  
 }
 
-const genJavascriptCode = (data) => {
-    var compDefName =data.name;
-    var ionicName = upperFist(data.name);
-    return `exports._${compDefName} = Ionic.${ionicName};  
-`;
+const genJavascriptCode = (name) => {
+    var compDefName =name;
+    var ionicName = upperFist(name);
+    console.log(`exports._${compDefName} = Ionic.${ionicName};  
+`);
 };
 
 function upperFist(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
 
 generateIonicTypes(process.argv[2] == "js");
